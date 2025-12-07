31bcf151a1902cf781d7f5ec81b4f9e167dc4d16
Jon Saw <jon.saw@gmail.com>
2025-12-08 00:31:26 +0800

    Add initial API authentication package structure

---
+++ packages/api_auth/lib/api_auth.dart
@@ -0,0 +1,7 @@
+export 'package:api_auth/__generated__/schema.schema.gql.dart';
+export 'package:api_auth/src/components/graphql/__generated__/api_version.data.gql.dart';
+export 'package:api_auth/src/components/graphql/__generated__/api_version.req.gql.dart';
+export 'package:api_auth/src/components/graphql/__generated__/api_version.var.gql.dart';
+export 'package:api_auth/src/components/graphql/__generated__/registration.req.gql.dart';
+export 'package:api_auth/src/components/graphql/__generated__/sign_in.req.gql.dart';
+export 'package:api_auth/src/components/graphql/__generated__/sign_out.req.gql.dart';
