abc54df58e3e6dfe5d125a7a68c78a02c2858f99
Jon Saw <jon.saw@gmail.com>
2025-12-11 09:27:37 +0800

    Management API add initial GraphQL schema and queries

---
+++ packages/api_management/lib/api_management.dart
@@ -0,0 +1,7 @@
+export 'package:api_management/__generated__/schema.schema.gql.dart';
+export 'package:api_management/src/components/graphql/__generated__/get_my_company.data.gql.dart';
+export 'package:api_management/src/components/graphql/__generated__/get_my_company.req.gql.dart';
+export 'package:api_management/src/components/graphql/__generated__/get_my_company.var.gql.dart';
+export 'package:api_management/src/components/graphql/__generated__/list_my_companies.data.gql.dart';
+export 'package:api_management/src/components/graphql/__generated__/list_my_companies.req.gql.dart';
+export 'package:api_management/src/components/graphql/__generated__/list_my_companies.var.gql.dart';
